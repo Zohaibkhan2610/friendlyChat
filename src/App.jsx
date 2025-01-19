b7cb8f0a93e4a04c84f9f2baa605ae044c0e258c
Zohaibkhan2610 <zohaibkhan2610765@gmail.com>
2025-01-19 10:30:31 +0500

    v0 init all setting

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaThumbsUp } from 'react-icons/fa'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">
+      Tailwind css and React router dom and React icons intalled successfully <FaThumbsUp/></h1>
+    </div>
+  )
+}
+
+export default App
